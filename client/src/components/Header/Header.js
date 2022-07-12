23645d109e9385ce2b99ba18651c182087c31af5
Kanishka <kanishka.bansode@gmail.com>
2022-07-12 09:10:59 +0530

    navbar done

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom';
 import {Home , HomeOutlined ,SearchOutlined,
@@ -7,21 +7,23 @@ import {Home , HomeOutlined ,SearchOutlined,
     Add,
     AddOutlined,
     Forum,
-    Campaign} from '@mui/icons-material'
+    Campaign,
+    CottageOutlined} from '@mui/icons-material'
 const Header = () => {
+    const [tab, setTab] = useState(window.location.pathname);
   return (
     <div className='HeaderWrapper'>
     <div className='header'>
-        <Link to="/">
-            <Home/>
+        <Link to="/" onClick={(e) => setTab('/')}>
+            {tab ==="/"? (<HomeOutlined style={{color:"rgb(255, 255, 255)"}} />) :(<Home/>) }
         </Link>
 
-        <Link to="/post/new">
-            <Add/>
+        <Link to="/post/new" onClick={(e) => setTab('/post/new')}>
+            {tab ==="/post/new"? (<AddOutlined style={{color:"rgb(255, 255, 255)"}} />) :(<Add/>) }
         </Link>
 
-        <Link to="/search">
-            <Search/>
+        <Link to="/search" onClick={(e) => setTab('/search')}>
+            {tab ==="/search"? (<SearchOutlined style={{color:"rgb(255, 255, 255)"}} />) :(<Search/>) }
         </Link>
 
         <a href='https://iiitp-civilised.netlify.app/' target="_blank">
@@ -32,8 +34,10 @@ const Header = () => {
             <Forum/>
         </a>
 
-        <Link to='/account'>
-            <AccountCircle/>
+        <Link to='/account' onClick={(e) => setTab('/account')}>
+            {tab ==="/account"? (<AccountCircleOutlined style={{color:"rgb(255, 255, 255)"}} />) :(<AccountCircle/>) }
+
+            
         </Link>
 
         
